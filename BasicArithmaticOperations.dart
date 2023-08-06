797d2556d0c27bcaec0f0b16ff937098d833388a
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-06 22:40:42 +0600

    Create BasicArithmaticOperations.dart
    Most basic 4 arithmatic operations

---
+++ BasicArithmaticOperations.dart
@@ -0,0 +1,17 @@
+void main(){
+  // In this file we will see most basic 4 operators used in Dart
+  // They are, Addition (+), Subtraction (-), Multiplication(*) & division(/)
+  int a = 19;
+  int b = 2;
+
+  // Addition
+
+  print("The result of ${a}+${b} is: ${a+b}");
+  //Subtraction
+  print("The result of ${a}-${b} is:${a-b}");
+  //Multiplication
+  print("The result of ${a}x${b} is:${a*b}");
+  ///Division
+  print("The result of ${a}/${b} is:${a/b}");
+
+}
