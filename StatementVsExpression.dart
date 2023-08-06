59a456ee3cda1887691e6bdeed3109d79f204215
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-06 19:38:48 +0600

    Create StatementVsExpression.dart

---
+++ StatementVsExpression.dart
@@ -0,0 +1,23 @@
+void main(){
+  //Statement is a command in Dart
+  //Writing statement means telling computer to do something
+  //For example we use print statement to show/output something
+  //statement ends with a ; (semicolon)
+  //Semicolon helps dart compiler to understand and execute the code properly
+  // Without semicolon error will be generate.
+
+  print("This is just a statement");
+  // We need not to use ; after the {} of complex statement like if....else
+  int age = 28;
+  if (age >=18) {
+    print("Adult");
+  }else {
+    print("Child");
+  }   // We did not use any semicolon after the closing curly braces here.
+
+  //Expression
+  //Expression does not do something, rather expression is a value that can
+  //be calculated.The values can be number, text or even a variable
+  // For example: 42, "Hello Dart", X etc.
+
+}
