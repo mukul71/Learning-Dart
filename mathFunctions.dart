44ea2d9006a8ea7e00986de68d7eda7d6f42ff7f
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-08 18:35:52 +0600

    Create mathFunctions.dart
    Some common math functions used in Dart.

---
+++ mathFunctions.dart
@@ -0,0 +1,13 @@
+// Dart has a long range of math function. To use dart's math functions
+// we need to import dart:math library first.
+//After importing we can use as below examples.
+
+import 'dart:math';
+
+void main (){
+  print(sin(45 * pi / 180));  //output: 0.7071067811865476
+  print(cos(135 * pi / 180)); //output: -0.7071067811865475
+  print(sqrt(2));             //output: 1.4142135623730951
+  print(max(5,10));
+  print(min(-5,-10));
+}
