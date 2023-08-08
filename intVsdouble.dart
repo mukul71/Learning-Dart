8192087789b7547bfd75e560c8e443cb27dcfa54
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-08 19:31:25 +0600

    Create intVsdouble.dart

---
+++ intVsdouble.dart
@@ -0,0 +1,23 @@
+void main() {
+// In your dart code, you can give a name to each piece of your data that you
+// you can refer later.
+//The name carries with it an associated type that denotes what sort of data
+// that name refers to, such number, text, or a date.
+
+// Variable
+// A variable works like a container that contains data and its type. It is
+// known as variable because its value can be changed. For example:
+
+  int number = 10; // now the number contains 10
+  number = 15;   // but now, same number contains 15.
+  print (number);
+
+  // The type int. stores Integer data.
+  // to store decimal data we use 'double' data type. For Example:
+
+  double cgpa = 3.5;
+  print("My CGPA is ${cgpa} in the final semester");
+
+  // One interesting example
+  print(10.isEven); // Can you tell what will be the output? 
+}
