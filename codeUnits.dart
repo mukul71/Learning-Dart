563a2fcf03944d2c1e27a58bb6919b65784bf54c
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-13 23:04:17 +0600

    Update codeUnits.dart
    Adding emoji in code

--- codeUnits.dart
+++ codeUnits.dart
@@ -8,4 +8,11 @@ void main (){
 
   //output is [72, 101, 108, 108, 111, 33] H = 72, e =101, l =108 and so on
   //.codeUnits can also be used to know number value of emojis as well.
+
+  // Reading emoji
+  //As I use windows, I can insert emoji by using the "Win + ." shortcut. 
+  const family = '👨‍👨‍👧‍👧';
+  print(family.runes); // output will be 128104, 8205, 128104, 8205, 128103, 8205, 128103. These stand for a man, woman, a girl and boy, 
+  //and all glued together with a character called Zero with Joiner or zWJ. 
+  print(family.length); 
 }
