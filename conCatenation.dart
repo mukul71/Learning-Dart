ffe54eff7faf93fff70fd759a2528ead0ca0345d
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-14 22:16:13 +0600

    Create conCatenation.dart
    ConCatenation

---
+++ conCatenation.dart
@@ -0,0 +1,10 @@
+void main (){
+  // In Dart, we can combine one string with another which is known as
+  //concatenation.
+  // we can do it just adding operation " + ", as below:
+
+  String message = 'Hello' + ' my name is ';
+  const name = 'John';
+  print (message += name);
+
+}
