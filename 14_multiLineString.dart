1d78f131434840370845524f871dcc3607dc3468
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-15 22:50:21 +0600

    Update 14_multiLineString.dart
    Multiple single lines in one line.

--- 14_multiLineString.dart
+++ 14_multiLineString.dart
@@ -8,4 +8,15 @@ void main () {
   doing this.''';
   print(bigString); //Output will be the same as you have written above. 
   // The three ''' signifies that this is a multi linge string. Three double """ will do the same thing. 
+  
+   //If you just want to use multiple lines
+  // in code but don’t want the output string to contain newline characters,
+  // then you can surround each line with single quotes:
+
+  const bigString2 = 'This is only'
+  'a single line '
+  'at '
+  'runtime.';
+  print(bigString2); // Output: This is only a single line at runtime.
+  
 }
