5e47ce3573b57e1849c840ede0d1be98c5831b09
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-15 21:57:23 +0600

    Create interPolation.dart
    Interpolation

---
+++ interPolation.dart
@@ -0,0 +1,11 @@
+void main () {
+  //Interpolation is a special syntax that makes string more readable to others
+  // In this syntax we replace some parts of string with other value.
+  //A '$' is used in front of the value that you want to insert.
+
+  String name = 'Aria';
+  int age = 10;
+  String gender = 'girl';
+
+  print('Hello, my name is $name, I am $age years old, I am a $gender');
+}
