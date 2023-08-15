fc4983b31e43e28bc2832c641a690d6e3a2458ce
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-15 22:40:08 +0600

    Create 14_multiLineString.dart
    multiLineString

---
+++ 14_multiLineString.dart
@@ -0,0 +1,11 @@
+void main () {
+  //we can use multi line string as below:
+
+  const bigString = ''' You can have a string
+  that contains multiple
+  lines
+  by
+  doing this.''';
+  print(bigString); //Output will be the same as you have written above. 
+  
+}
