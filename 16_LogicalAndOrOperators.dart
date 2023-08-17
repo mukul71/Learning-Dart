3c5b343dcdb2a2c654887c583ca9b80888a0d238
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-17 22:28:13 +0600

    Update 16_LogicalAndOrOperators.dart
    OR Operator

--- 16_LogicalAndOrOperators.dart
+++ 16_LogicalAndOrOperators.dart
@@ -7,4 +7,14 @@ void main() {
   print(willCycling); //Output will be:  True
    //If either isSunny or isFinished were false ,
   // then willGoCycling would also be false .
+
+   //OR operator
+  //If only one of two conditions
+  //needs to be true in order for the result to be true, this is an example of
+  // a Boolean OR   operation.
+
+  const willTravel = true;
+  const seeBird = false;
+  const takePhotograph = willTravel || seeBird;
+  print(takePhotograph);
 }
