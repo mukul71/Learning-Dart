80daa5bf898b00b1588ed7e48c5e7bea0d12a23c
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-17 22:21:35 +0600

    Create 16_AndOperator.dart
    And Operator

---
+++ 16_AndOperator.dart
@@ -0,0 +1,9 @@
+void main() {
+  // To use and operator in Dart, two conditions must be ture to become a
+  //result ture.
+  const isSunny = true;
+  const workFinished = true;
+  const willCycling = isSunny && workFinished;
+  print(willCycling); //Output will be:  True
+
+}
