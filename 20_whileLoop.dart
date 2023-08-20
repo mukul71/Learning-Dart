2a364c567ace8851acc23d090910a12248ec4b32
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-20 23:28:55 +0600

    Update 20_whileLoop.dart

--- 20_whileLoop.dart
+++ 20_whileLoop.dart
@@ -1,4 +1,7 @@
 void main(){
+  //In while loop, condition is checked first
+  //Then statement is printed vased on the comparison of the condition.
+  //Finally, value of checked variable is increased.
   var num = 1;
   while(num <10){
     print('$num,Hello, Dart');
