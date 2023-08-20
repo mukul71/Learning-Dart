ab177b41acf84c639b035bf91fd92af8b15f2070
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-20 23:36:35 +0600

    Update 20_whileLoop.dart
    do-while loop

--- 20_whileLoop.dart
+++ 20_whileLoop.dart
@@ -7,4 +7,12 @@ void main(){
     print('$num,Hello, Dart');
     num++;
   }
+  // on the other hand do-while loop prints the statement first but check the
+  //condition at last stage of the action.
+
+  var sum =1;
+  do{
+    print(sum);
+    sum++;
+  }while(sum <10);
 }
