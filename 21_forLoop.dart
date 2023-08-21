111458ca8d10f633636f5be8eceea66fae8ee626
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-21 22:10:11 +0600

    Update 21_forLoop.dart

--- 21_forLoop.dart
+++ 21_forLoop.dart
@@ -6,4 +6,18 @@ void main() {
   for (var i =0;i<10;i++) {
     print(i);
   }
+  //var i = 0 (initialization): Before the loop starts, you create a counter
+  // variable to keep track of how many times you’ve looped. You could call the
+  // variable anything, but i is commonly used as an abbreviation for index.
+  // You then initialize it with some value; in this case, 0 .
+
+  //i < 10 (condition): This is the condition that the for loop will check before
+  // every iteration of the loop. If it’s   // true , then it will run the code
+  // inside the braces. But if it’s false , then the loop will end.
+
+  //i++ (action): The action runs at the end of every iteration, usually to 
+  // update the loop index value. It’s common to increment by 1 using i++ 
+  // but you could just as easily use i += 2 to increment by 2 or i-- to 
+  // decrement by 1 .
+}
 }
