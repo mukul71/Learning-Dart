2231204fd0ee0651e5be5ff6fa470ae9ec841633
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-22 22:48:42 +0600

    Update 22_parameter.dart

--- 22_parameter.dart
+++ 22_parameter.dart
@@ -2,6 +2,7 @@ void main() {
   //In dart function you can use number of parameters. 
   //Argument should send as per the sequence of the parameter,or will get error
   //you need to separate parameters by commas (if you have more than one parameters)
+  //You need to send the exact number of arguments as the number of parameters. 
   
   void personAndPet(String person,String pet) {
     print('Hello, $person, and your furry friend, $pet.');
