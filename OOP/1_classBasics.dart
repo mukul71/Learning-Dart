070c0aefecc565ca45da0ff5ab6404ef5dd075ca
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-25 22:23:51 +0600

    Create 1_classBasics.dart

---
+++ OOP/1_classBasics.dart
@@ -0,0 +1,15 @@
+void main() {
+  //Classes are a core component of object-oriented programming. They’re used to
+  // combine data and functions inside a single structure Classes are like
+  // architectural blueprints that tell the system how to make an object,
+  // where an object is the actual data stored in the computer’s memory.
+  //Functions inside a class are known as methods, whereas constructors are
+  // special methods you use to create objects from the class
+  //Class should be above or below the main metheod.
+
+}
+class User() {
+  int id =0;
+  String name = '';
+}
+// name of the above class is User that has 2 properties: id and name 
