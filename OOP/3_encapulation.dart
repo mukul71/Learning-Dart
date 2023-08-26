dda3cfcb7c24a15e0f4b83b5ec9c2238b4c26352
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-26 22:54:53 +0600

    Create 3_encapulation.dart
    Encapsulation

---
+++ OOP/3_encapulation.dart
@@ -0,0 +1,24 @@
+void main() {
+  print("Encapsulation");
+}
+//One of the core tenets of object-oriented programming is known as encapsulation.
+//his is
+// the principle of hiding the internal data and logic in a class from the
+// outside world.
+//How do you accomplish encapsulation in Dart?
+//You can make a variable private in Dart by prefixing the name with an
+// underscore
+
+class Password {
+  String _plainText = 'pass123';
+
+//The name _value begins with an underscore, so it’s private. That means there’s
+// no way to access the user ID and name outside of the class, which makes
+// your object kind of useless. You can solve this problem by adding a getter.
+//A getter is a special method that returns the value of a private field variable.
+//It’s the public face of a private variable.
+//getter methods starts with ' get ' like 'getColor' or 'getWidth'.
+  String getPlaintext() {
+    return _plainText;
+  }
+}
