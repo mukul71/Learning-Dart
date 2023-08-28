572725d57cf5656f8419eaf51294ad30d37e36e4
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-28 22:23:35 +0600

    Create 5_constructors.dart
    Constructors

---
+++ OOP/5_constructors.dart
@@ -0,0 +1,24 @@
+void main(){
+  print('Constructors');
+
+  // constructors are method that create or constuct instances of a class
+  // constructors build new objects
+  // constructors have the same name as the class
+  // return type of a constructor is as same as the class
+
+  print('Default constructor');
+}
+
+  //When you don’t specify a constructor, Dart provides a default constructor
+  // that takes no parameters and just returns an instance of the class, as
+
+  class Address {
+    var value = '';
+  }
+
+ // is equialent as below:
+
+   class Address{
+    Address(); // constructor
+  var value = '';
+  }
