56edc1ad0e88a902418a22944ec0a560cd70daed
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-30 22:57:08 +0600

    Update 6_staticKeyword.dart

--- OOP/6_staticKeyword.dart
+++ OOP/6_staticKeyword.dart
@@ -5,11 +5,20 @@ class Practice {
   }
 }
 
-// Putting static in front of a member variable or method causes the variable 
+class Colors {
+  static const int red = 0xFFD13F13;
+  static const int purple = 0xFF8107D9;
+  static const int blue = 0xFF1432C9;
+}
+
+// Putting static in front of a member variable or method causes the variable
 // or method to belong to the class rather than the instance.
 
 void main() {
   final value = Practice.myNum;
   final value2 = Practice.myMethod();
   print(value);
+
+  final backgrounColor = Colors.purple;
+  print(backgrounColor);
 }
