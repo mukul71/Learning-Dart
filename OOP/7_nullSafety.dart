ff9099e247a89142ce7e7cbab960ad8298eb3d4c
Mukul Rahman <mukul71@users.noreply.github.com>
2023-08-31 23:20:08 +0600

    Update 7_nullSafety.dart

--- OOP/7_nullSafety.dart
+++ OOP/7_nullSafety.dart
@@ -1,5 +1,11 @@
 void main() {
   print('Null safety');
+  
   //Dart took null out of list from the version 2.2
   //Null means “no value” or “absence of a value”
+  //if you can have a dedicated value called null , which everyone already
+  //understands to mean “no value”, then you don’t need to add comments explaining 
+  //what it means
+  int postalCode = null;  //It’s obvious here that there’s no postal code. 
+  
 }
