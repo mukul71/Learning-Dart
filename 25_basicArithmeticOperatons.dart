c7642c03bcc3c3ed2adeea041cd3e718f67469c6
Mukul Rahman <mukul71@users.noreply.github.com>
2023-10-06 22:55:29 +0600

    Create 25_basicArithmeticOperatons.dart
    Basic Arithmetic Operations

---
+++ 25_basicArithmeticOperatons.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+void main() {
+  print('sin = ${sin(45 * pi / 180)}');
+  print('Square of 2 is: ${sqrt(2)}');
+  print('Cosine is : ${cos(45 * pi / 180)}');
+  print('Max of 5 & 2 is ${max(5, 2)}');
+  print('Min of -5 & 2 is ${min(-5, 2)}');
+}
