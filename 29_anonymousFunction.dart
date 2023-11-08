bc90c666bf62c22937b2d62f42642620b91de211
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-08 14:56:30 +0600

    Update 29_anonymousFunction.dart
    consolidating collection

--- 29_anonymousFunction.dart
+++ 29_anonymousFunction.dart
@@ -7,4 +7,10 @@ void main() {
     looped.add(x * x);
   }
   print(looped); //output: [4,16,36,64]
+    print(looped); //output: [4,16,36,64]
+  //
+  print('Consolidating collections');
+  const evens = [2, 4, 6, 8, 10, 12];
+  final total = evens.reduce((sum, element) => sum + element);
+  print(total);// total will be 42
 }
