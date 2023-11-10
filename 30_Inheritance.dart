60b439e0e66d9ace0409cecadcba47215934d009
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-10 15:59:24 +0600

    Update 30_Inheritance.dart
    person and user classes created

--- 30_Inheritance.dart
+++ 30_Inheritance.dart
@@ -1,3 +1,29 @@
 void main() {
   print('Inheritance');
+
+
+  
+}
+enum Grade = {A,B,C,C,E,F};
+
+class Person{
+  Person(this.giveName,this.surName);
+  String giveName;
+  String surName;
+
+  String get fullName => '$giveName $surName';
+
+  @override
+  String toString =>fullName;
+}
+
+class Student{
+  Student(this.giveName, this.surName)
+  String giveName;
+  String surName;
+  var grades = <Grade>[];
+  String get fullName =>'$giveName $surName';
+  @override
+  String toString()=>fullName();
+  
 }
