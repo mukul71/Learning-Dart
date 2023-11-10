418fa958070e35518564ab95fe67783e051f93ba
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-10 16:32:13 +0600

    Update 30_Inheritance.dart
    inheritance

--- 30_Inheritance.dart
+++ 30_Inheritance.dart
@@ -1,37 +1,38 @@
 void main() {
   print('Inheritance');
 
+  final jon = Person('Jon', 'Smith');
+  final jane = Student('Jane', 'Smith');
+  print(jon);
+  print(jane);
+}
 
-  
-}//Main Class 
+enum Grade { A, B, C, E, F }
 
-enum Grade = {A,B,C,C,E,F};
-
-class Person{
-  Person(this.giveName,this.surName);
+class Person {
+  Person(this.giveName, this.surName);
   String giveName;
   String surName;
 
   String get fullName => '$giveName $surName';
 
   @override
-  String toString => fullName;
+  String toString() => fullName;
 }
 
-class Student{
+class Student {
   Student(this.giveName, this.surName);
   String giveName;
   String surName;
   var grades = <Grade>[];
-  String get fullName =>'$giveName $surName';
-  
+  String get fullName => '$giveName $surName';
+
   @override
-  String toString() => fullName();
-
-  //Inheriting to student from person
-
-class Student2 extends Person{
-  Student2 (String giveName, String surName) : super(giveName,surName);
-  var grades = <Grade>[];
+  String toString() => fullName;
 }
+//Inheriting to student from person
+
+class Student2 extends Person {
+  Student2(String giveName, String surName) : super(giveName, surName);
+  var grades = <Grade>[];
 }
